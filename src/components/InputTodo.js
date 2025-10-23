39c19e42119d10ff1e787376bc41985760f97a5d
Phixah <nicknameabc4@gmail.com>
2025-10-23 11:35:14 +0700

    modified:   src/components/InputTodo.js

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -7,7 +7,7 @@ const InputTodo = () => {
     e.preventDefault();
     try {
       const body = { description };
-      await fetch(`"${process.env.REACT_APP_API_URL}/todos"`, {
+      await fetch(`${process.env.REACT_APP_API_URL}/todos`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(body),
